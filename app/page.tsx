93ea17113d479c866f07c18a86e142bac2d2bf20
aarslantas <a_arslantas69@hotmail.com>
2024-11-21 22:24:19 +0300

    edit tailwind.config

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <div className="text-3xl font-black text-white">Welcome to Nextjs 15 </div>
+    <div className="text-3xl font-black text-primary-bayburt">
+      Welcome to Nextjs 15{" "}
+    </div>
   );
 }

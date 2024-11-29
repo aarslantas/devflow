f48916d8f46f37470a36c7e4e0866222dc56bd7c
aarslantas <a_arslantas69@hotmail.com>
2024-11-29 19:39:15 +0300

    import shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,29 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 export default function Home() {
   // TODO:asdasdasd
   return (
     <>
       <h1 className="h1-bold">Welcome to Nextjs 15 </h1>
       <h1 className="h1-bold font-space-grotesk">Welcome to Nextjs 15 </h1>
+      <DropdownMenu>
+        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </>
   );
 }

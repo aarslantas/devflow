46e907f240d5ecfa6c3bc0150863fc70bca7c5eb
aarslantas <a_arslantas69@hotmail.com>
2024-11-30 12:10:09 +0300

    add auth route

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Navbar from "@/components/ui/navigation/navbar";
+
+const RootLayout = () => {
+  return (
+    <div>
+      <Navbar />
+      RootLayout
+    </div>
+  );
+};
+
+export default RootLayout;

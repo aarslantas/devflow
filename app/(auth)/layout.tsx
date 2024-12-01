88f3ba72c5f45efd58c2d1f363678849ac34ce79
aarslantas <a_arslantas69@hotmail.com>
2024-12-01 22:29:08 +0300

    update auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,30 @@
+import Image from "next/image";
 import React, { ReactNode } from "react";
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
-  return <div>AuthLayout {children}</div>;
+  return (
+    <main className="flex min-h-screen item-center justify-center bg-auth-light dark:bg-auth-dark bg-cover bg-center bg-no-repeat px-4 py-10">
+      <section className="light-border background-light800_dark200 shadow-light100_dark100 min-w-full rounded-[10px] border px-4 py-10 shadow-md sm:min-w-[520px] sm:px-8">
+        <div className="flex items-center justify-between gap-2">
+          <div className="space-y-2.5">
+            <h1 className="h2-bold tect-dark100_light900">Join DevFlow</h1>
+            <p className="paragraf-regular text-dark500_light400">
+              To get Your questions answered{" "}
+            </p>
+          </div>
+          <Image
+            src="images/site-logo.svg"
+            alt="DevFlow Logo"
+            width={50}
+            height={50}
+            className="object-contain"
+          />
+        </div>
+        {children}
+        <p>Social Auth</p>
+      </section>
+    </main>
+  );
 };
 
 export default AuthLayout;

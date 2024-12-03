ddfd8e85aa9fa729ff5a9bb21d2aa6402c8f352d
aarslantas <a_arslantas69@hotmail.com>
2024-12-03 09:11:32 +0300

    add auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,6 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
 
-import Navbar from "@/components/ui/navigation/navbar";
-
 const inter = localFont({
   src: "./fonts/interVF.ttf",
   variable: "--font-inter",

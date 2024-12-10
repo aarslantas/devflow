6d2156683ccf7c5e08a6b14c16ba35fa5e92e4aa
aarslantas <a_arslantas69@hotmail.com>
2024-12-10 23:39:59 +0300

    change side

--- components/navigation/navbar/MobileNavigation.tsx
+++ components/navigation/navbar/MobileNavigation.tsx
@@ -6,22 +6,38 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import { Link } from "lucide-react";
+import Image from "next/image";
 
 const MobileNavigation = () => {
   return (
     <Sheet>
       <SheetTrigger>Open</SheetTrigger>
-      <SheetContent>
-        <SheetHeader>
-          <SheetTitle>
-            Are you absolutely sure?
-          </SheetTitle>
-          <SheetDescription>
-            This action cannot be undone. This
-            will permanently delete your account
-            and remove your data from our servers.
-          </SheetDescription>
-        </SheetHeader>
+      <Image
+        src="/icons/hamburger.svg"
+        width={36}
+        height={36}
+        alt="Menu"
+        className="invert-colors sm:hidden"
+      />
+      <SheetContent
+        side="left"
+        className="background-light900_dark200"
+      >
+        <SheetTitle className="hidden">
+          Navigation
+        </SheetTitle>
+        <Link
+          href="/"
+          className="flex items-center gap-1"
+        >
+          <Image
+            src="/images/site-logo.svg"
+            width={23}
+            height={23}
+            alt="Logo"
+          />
+        </Link>
       </SheetContent>
     </Sheet>
   );

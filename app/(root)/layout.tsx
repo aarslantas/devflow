e215dacfd6ec2a81eabcc18d2d022693a6b9830e
aarslantas <a_arslantas69@hotmail.com>
2024-12-14 22:57:53 +0300

    add LeftSidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/navigation/navbar";
+import LeftSidebar from "@/components/navigation/navbar/LeftSidebar";
 import React from "react";
 
 const Layout = ({
@@ -7,10 +8,17 @@ const Layout = ({
   children: React.ReactNode;
 }>) => {
   return (
-    <div>
+    <main className="background-light850_dark100 relative">
       <Navbar />
-      {children}
-    </div>
+      <div>
+        <LeftSidebar />
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
+          <div className="mx-auto w-full max-w-5xl">
+            {children}
+          </div>
+        </section>
+      </div>
+    </main>
   );
 };
 

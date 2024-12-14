c7fcb3e73a801db0b2c3a231d19eb06b704c33ec
aarslantas <a_arslantas69@hotmail.com>
2024-12-14 23:09:02 +0300

    update LetftSidebar

--- components/navigation/navbar/NavLinks.tsx
+++ components/navigation/navbar/NavLinks.tsx
@@ -10,7 +10,7 @@ import React from "react";
 const NavLinks = ({
   isMobileNav = false,
 }: {
-  isMobileNav: boolean;
+  isMobileNav?: boolean;
 }) => {
   const pathName = usePathname();
   const userId = 1;

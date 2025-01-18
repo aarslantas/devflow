849da4f3b29359c28b0b55d8d7edb2850aa26608
aarslantas <a_arslantas69@hotmail.com>
2025-01-18 15:23:02 +0300

    remove unused answer model and add tag-question model with schema definition and interface

---
+++ database/tag-question.model.ts
@@ -0,0 +1,36 @@
+import {
+  model,
+  models,
+  Schema,
+  Types,
+} from "mongoose";
+
+import { ITag } from "./tag.model";
+
+export interface ITagQuestion {
+  tag: Types.ObjectId;
+  question: Types.ObjectId;
+}
+
+const ITagQuestionSchema =
+  new Schema<ITagQuestion>(
+    {
+      tag: {
+        type: Schema.Types.ObjectId,
+        ref: "Tag",
+        required: true,
+      },
+      question: {
+        type: Schema.Types.ObjectId,
+        ref: "Question",
+        required: true,
+      },
+    },
+    { timestamps: true }
+  );
+
+const TagQuestion =
+  models?.TagQuestion ||
+  model<ITagQuestion>("", ITagQuestionSchema);
+
+export default TagQuestion;

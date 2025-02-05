31272b259d01ac2bbdf96eb602733d6853b41e43
aarslantas <a_arslantas69@hotmail.com>
2025-02-05 22:27:41 +0300

    add collection model with schema definition and interface

---
+++ database/collection.model.ts
@@ -0,0 +1,33 @@
+import {
+  model,
+  models,
+  Schema,
+  Types,
+} from "mongoose";
+
+export interface ICollection {
+  author: Types.ObjectId;
+  question: Types.ObjectId;
+}
+
+const CollectionSchema = new Schema<ICollection>(
+  {
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    question: {
+      type: Schema.Types.ObjectId,
+      ref: "Question",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Collection =
+  models?.Collection ||
+  model<ICollection>("", CollectionSchema);
+
+export default Collection;

751fa638b84261bbb530eaa8478c793885157106
aarslantas <a_arslantas69@hotmail.com>
2025-02-23 16:19:14 +0300

    add API routes for ticket management; implement GET, PUT, and POST methods

--- app/api/tickets/route.ts
+++ app/api/tickets/route.ts
@@ -5,3 +5,35 @@ import tickets from "@/app/database";
 export async function GET() {
   return NextResponse.json(tickets);
 }
+
+export async function POST(request: Request) {
+  const newTicket = await request.json();
+  tickets.push({
+    id: tickets.length + 1,
+    ...newTicket,
+  });
+  return NextResponse.json(newTicket, {
+    status: 201,
+  });
+}
+
+export async function GET_BY_ID(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { searchParams } = new URL(request.url);
+
+  const id = params.id;
+  const ticket = tickets.find(
+    (ticket) => ticket.id === parseInt(id)
+  );
+
+  if (!ticket) {
+    return NextResponse.json(
+      { error: "Ticket not found" },
+      { status: 404 }
+    );
+  }
+
+  return NextResponse.json(ticket);
+}

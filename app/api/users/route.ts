8f228603b4795dda3bd008a501c5e3af790036ab
aarslantas <a_arslantas69@hotmail.com>
2025-02-28 19:34:46 +0300

    add GET route for user API; implement functionality to retrieve all users

---
+++ app/api/users/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+
+import User from "@/database/user.model";
+import { handleError } from "@/lib/handlers/error";
+import dbConnect from "@/lib/mongoose";
+import { APIErrorResponse } from "@/types/global";
+
+export async function GET() {
+  try {
+    await dbConnect();
+    const users = await User.find();
+    return NextResponse.json({
+      succes: true,
+      data: users,
+      status: 200,
+    });
+  } catch (error) {
+    return handleError(
+      error,
+      "api"
+    ) as APIErrorResponse;
+  }
+}

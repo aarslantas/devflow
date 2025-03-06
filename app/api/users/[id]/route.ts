e04a6bde55124543ec064ad521a88ec50254ffdc
aarslantas <a_arslantas69@hotmail.com>
2025-03-06 19:27:37 +0300

    add GET route for user API; implement functionality to retrieve user by ID with error handling

---
+++ app/api/users/[id]/route.ts
@@ -0,0 +1,44 @@
+import { NextResponse } from "next/server";
+
+import User from "@/database/user.model";
+import { handleError } from "@/lib/handlers/error";
+import {
+  NotFoundError,
+  ValidationError,
+} from "@/lib/http-errors";
+import dbConnect from "@/lib/mongoose";
+import { UserSchema } from "@/lib/validation";
+import { APIErrorResponse } from "@/types/global";
+
+// Get User by ID   -> /api/users/[id]
+export async function GET(
+  _: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = await params;
+
+  if (!id) {
+    throw new NotFoundError("User");
+  }
+
+  try {
+    await dbConnect();
+    const user = await User.findById(id);
+    if (!user) {
+      throw new NotFoundError("User not found");
+    }
+
+    return NextResponse.json(
+      {
+        success: true,
+        data: user,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    return handleError(
+      error,
+      "api"
+    ) as APIErrorResponse;
+  }
+}

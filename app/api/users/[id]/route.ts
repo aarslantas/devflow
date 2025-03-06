ff37ff89b63d30bb330d4c2168889a0b12eb4cc8
aarslantas <a_arslantas69@hotmail.com>
2025-03-06 19:30:48 +0300

    add DELETE route for user API; implement functionality to delete user by ID with error handling

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -2,12 +2,8 @@ import { NextResponse } from "next/server";
 
 import User from "@/database/user.model";
 import { handleError } from "@/lib/handlers/error";
-import {
-  NotFoundError,
-  ValidationError,
-} from "@/lib/http-errors";
+import { NotFoundError } from "@/lib/http-errors";
 import dbConnect from "@/lib/mongoose";
-import { UserSchema } from "@/lib/validation";
 import { APIErrorResponse } from "@/types/global";
 
 // Get User by ID   -> /api/users/[id]
@@ -32,6 +28,39 @@ export async function GET(
       {
         success: true,
         data: user,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    return handleError(
+      error,
+      "api"
+    ) as APIErrorResponse;
+  }
+}
+
+// Delete User by ID -> /api/users/[id]
+export async function DELETE(
+  _: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = await params;
+
+  if (!id) {
+    throw new NotFoundError("User");
+  }
+
+  try {
+    await dbConnect();
+    const user = await User.findByIdAndDelete(id);
+    if (!user) {
+      throw new NotFoundError("User not found");
+    }
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: "User deleted successfully",
       },
       { status: 200 }
     );

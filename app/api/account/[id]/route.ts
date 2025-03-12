644b459c019d9c16299d3dbfca0b56dc9cf1508c
aarslantas <a_arslantas69@hotmail.com>
2025-03-12 22:47:21 +0300

    add PUT route for account API; implement account update by ID with validation and error handling

--- app/api/account/[id]/route.ts
+++ app/api/account/[id]/route.ts
@@ -81,3 +81,55 @@ export async function DELETE(
     ) as APIErrorResponse;
   }
 }
+
+// Update User by ID -> /api/users/[id]
+export async function PUT(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+
+  if (!id) {
+    throw new NotFoundError("Account");
+  }
+
+  try {
+    await dbConnect();
+
+    const body = await request.json();
+    const validatedData =
+      AccountSchema.partial().safeParse(body);
+
+    if (!validatedData.success) {
+      throw new ValidationError(
+        validatedData.error.flatten().fieldErrors
+      );
+    }
+
+    const updatedAccount =
+      await User.findByIdAndUpdate(
+        id,
+        validatedData,
+        { new: true }
+      );
+
+    if (!updatedAccount) {
+      throw new NotFoundError(
+        "Account not found"
+      );
+    }
+
+    return NextResponse.json(
+      {
+        success: true,
+        data: updatedAccount,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    return handleError(
+      error,
+      "api"
+    ) as APIErrorResponse;
+  }
+}

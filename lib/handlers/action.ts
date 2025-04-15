5de86f5d8e8711d0cf0e39999f47b5d75c8849bd
aarslantas <a_arslantas69@hotmail.com>
2025-04-15 17:20:33 +0300

    refactor action handler; enhance schema validation and add session management

--- lib/handlers/action.ts
+++ lib/handlers/action.ts
@@ -1,9 +1,59 @@
 "use server";
 
-import { ZodSchema } from "zod";
+import { Session } from "next-auth";
+import { ZodError, ZodSchema } from "zod";
+
+import { auth } from "@/auth";
+
+import {
+  UnauthorizedError,
+  ValidationError,
+} from "../http-errors";
+import dbConnect from "../mongoose";
 
 type ActionOptions<T> = {
   params?: T;
   schema?: ZodSchema<T>;
   authorize?: boolean;
 };
+
+async function action<T>({
+  params,
+  schema,
+  authorize = false,
+}: ActionOptions<T>) {
+  // Validate schema if provided
+  if (schema && params) {
+    try {
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return new ValidationError(
+          error.flatten().fieldErrors as Record<
+            string,
+            string[]
+          >
+        );
+      } else {
+        return new Error(
+          "schema validation failed"
+        );
+      }
+    }
+  }
+
+  let session: Session | null = null;
+
+  if (authorize) {
+    session = await auth();
+
+    if (!session) {
+      return new UnauthorizedError();
+    }
+  }
+
+  await dbConnect();
+
+  return { params, session };
+}
+
+export default action;
